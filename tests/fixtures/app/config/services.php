8ded11c2b6cb78d348d913d2a9398247d8158ef9
slawek <slawomir.zytko@gmail.com>
2014-04-29 12:30:53 +0200

    Composer fix;

--- tests/fixtures/app/config/services.php
+++ tests/fixtures/app/config/services.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'UrlServiceProvider' => '/var/www/vegas/vegas-cmf-core/tests/fixtures/app/services/UrlServiceProvider.php',
-  'MongoServiceProvider' => '/var/www/vegas/vegas-cmf-core/tests/fixtures/app/services/MongoServiceProvider.php',
-  'AssetsServiceProvider' => '/var/www/vegas/vegas-cmf-core/tests/fixtures/app/services/AssetsServiceProvider.php',
-  'CollectionManagerServiceProvider' => '/var/www/vegas/vegas-cmf-core/tests/fixtures/app/services/CollectionManagerServiceProvider.php',
+  'MongoServiceProvider' => '/var/www/vegas-cmf/vegas-cmf-core/tests/fixtures/app/services/MongoServiceProvider.php',
+  'CollectionManagerServiceProvider' => '/var/www/vegas-cmf/vegas-cmf-core/tests/fixtures/app/services/CollectionManagerServiceProvider.php',
+  'UrlServiceProvider' => '/var/www/vegas-cmf/vegas-cmf-core/tests/fixtures/app/services/UrlServiceProvider.php',
+  'AssetsServiceProvider' => '/var/www/vegas-cmf/vegas-cmf-core/tests/fixtures/app/services/AssetsServiceProvider.php',
 );
