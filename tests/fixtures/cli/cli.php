0549566b3db5c38c17f119c5e0677070ff950d6c
Arek <arkadiusz.ostrycharz@gmail.com>
2014-06-03 10:49:26 +0200

    cli rollback

---
+++ tests/fixtures/cli/cli.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+echo PHP_EOL;
+try {
+    require APP_ROOT . '/../../vendor/autoload.php';
+    require APP_ROOT . '/cli/Bootstrap.php';
+    $config = require APP_ROOT . '/app/config/config.php';
+
+    $bootstrap = new \Vegas\Cli\Bootstrap(new \Phalcon\Config($config));
+    $bootstrap->setArguments($argv);
+
+    $bootstrap->setup()->run();
+} catch (\Exception $ex) {
+    echo "\033[47m\033[0;31m" . $ex->getMessage() . "\033[0m";
+    echo PHP_EOL;
+    echo PHP_EOL;
+    echo sprintf("\033[47m\033[0;31m%s\033[0m", "Use -h (--help) option for more help");
+}
+echo PHP_EOL;
+echo PHP_EOL;
