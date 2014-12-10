8a524a7eba6712b391aa2ee0ec339a30d7af3745
Mateusz Aniolek <matty201@gmail.com>
2014-12-10 17:09:16 +0100

    added arguments for sorting table, tests

--- tests/fixtures/app/config/config.php
+++ tests/fixtures/app/config/config.php
@@ -37,6 +37,7 @@ return array(
         "dbname" => "vegas_test",
         "port" => 3306,
         "username" => "root",
+        "password" => "root",
         "options" => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
         )
---
+++ tests/fixtures/app/config/modules.php
@@ -0,0 +1,17 @@
+<?php return array (
+  'Test' => 
+  array (
+    'className' => 'Test\\Module',
+    'path' => '/var/www/core/tests/fixtures/app/modules/Test/Module.php',
+  ),
+  'Example' => 
+  array (
+    'className' => 'Example\\Module',
+    'path' => '/var/www/core/tests/fixtures/app/modules/Example/Module.php',
+  ),
+  'Foo' => 
+  array (
+    'className' => 'Foo\\Module',
+    'path' => '/var/www/core/tests/fixtures/app/modules/Foo/Module.php',
+  ),
+);
