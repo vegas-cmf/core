36b4c99eb59f46f47476495e0e2d075723015a8f
Arek <arkadiusz.ostrycharz@gmail.com>
2015-03-26 12:20:26 +0100

    updated author tag

--- tests/Bootstrap/ErrorHandlerInitializerTraitTest.php
+++ tests/Bootstrap/ErrorHandlerInitializerTraitTest.php
@@ -1,13 +1,7 @@
 <?php
 /**
- * This file is part of Vegas package
- *
- * @author Slawomir Zytko <slawek@amsterdam-standard.pl>
- * @copyright Amsterdam Standard Sp. Z o.o.
- * @homepage http://vegas-cmf.github.io
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @author Arkadiusz Ostrycharz <aostrycharz@amsterdam-standard.pl>
+ * @copyright (c) 2015, Amsterdam Standard
  */
 
 namespace Vegas\Tests\Bootstrap;
